b99acd87cb054f8af717c1d71e6abd6d49022dc3
halfice <axix.szabist@gmail.com>
2018-09-25 15:14:49 +0400

    Final work
    Final work

--- src/webparts/wPprojectdetails/components/IWPprojectdetailsProps.ts
+++ src/webparts/wPprojectdetails/components/IWPprojectdetailsProps.ts
@@ -12,14 +12,27 @@ export interface IWPprojectdetailsProps {
   ProjectManager: string;
   ProjectTeam: string;
   showPanel: boolean;
-  _items:Array<object>;
-  SelectedItemId:number;
-  SelectedItemArray:Array<object>;
-  AuthorDisplayName:string;
-  BusinessManagerName:string;
-  TeamMembersName:string;
-  UserArray:Array<object>;
-  UserIds:Array<string>;
+  _items: Array<object>;
+  SelectedItemId: number;
+  SelectedItemArray: Array<object>;
+  AuthorDisplayName: string;
+  BusinessManagerName: string;
+  TeamMembersName: string;
+  UserArray: Array<object>;
+  UserIds: Array<string>;
+  PersonaArray: Array<object>;
+  PersonNameArray: Array<string>;
+
+  ProjectOpenAirId: string;
+  ProjectCode: string;
+  Client: string;
+  ClientContact: string;
+  ProjectStartDate: string;
+  ProjectPlannedHours: string;
+  ProjectPlannedDays: string;
+  ProjectReportingRequirements: string;
+  ProjectDeliverables: string;
+  ProjectUsefulLinks: string;
 
 }
 
